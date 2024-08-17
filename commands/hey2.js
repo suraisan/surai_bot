6b51d99139beb844a62976cbce8e39a9c8724cc5
suraisan <suraimu339@gmail.com>
2024-08-17 17:02:26 +0900

    コメントの削除

--- commands/hey2.js
+++ commands/hey2.js
@@ -1,8 +1,5 @@
-// SlashCommandBuilder という部品を discord.js からインポートしています。
-// これにより、スラッシュコマンドを簡単に構築できます。
 const { SlashCommandBuilder } = require('discord.js');
 
-// 以下の形式にすることで、他のファイルでインポートして使用できるようになります。
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('かも')
@@ -11,12 +8,3 @@ module.exports = {
 		await interaction.reply('うまそう');
 	},
 };
-
-// module.exportsの補足
-// キー・バリューの連想配列のような形で構成されています。
-//
-// module.exports = {
-//    キー: バリュー,
-//    キー: バリュー,
-// };
-//
